bb01ab092e3c6ba99fbbc225cf2a8b371904b38b
Lacey Murphy <lneagle@gmail.com>
2025-07-21 10:54:25 -0400

    to do list functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newTaskText = document.querySelector("#new-task-description").value;
+    buildToDo(newTaskText);
+  });
+
+  function buildToDo(task) {
+    const taskList = document.querySelector("#tasks");
+    const newTask = document.createElement("li");
+    newTask.textContent = task;
+    taskList.append(newTask);
+  }
 });
